8af6eae7b890ac4c646670801f24c31fb9f062da
ivan-lozhkin <139210681+ivan-lozhkin@users.noreply.github.com>
2024-11-11 19:27:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-def print_params(a, b, c ):
+def print_params(a = 1, b = 'строка', c = True):
     print(a,b,c)
 values_list = ['a', 20.5, False]
 values_dict = {'a': 5, 'b': 'int', 'c': True}
 
 values_list_2 = [60.5, 'int']
 
-# print_params(1,2,3)
-# print_params()
-# print_params(b = 25)
-# print_params(c = [1,2,3])
+print_params(1,2,3)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
 print_params(*values_list)
 print_params(**values_dict)
 print_params(*values_list_2, 42)
